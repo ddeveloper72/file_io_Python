aa1a5040179b970a8d2202de51819b5d015e25bc
Duncan Falconer <duncan.niamh@gmail.com>
2018-06-16 12:34:42 +0000

    Absolute and relative paths

--- quiz.py
+++ quiz.py
@@ -1,4 +1,16 @@
-f = open('data.txt', 'r')
-lines = f.read()
+f = open('files/relative_data.txt', 'r') #relative file path (relative to this file that were using)
+
+
+"""an absolute path starts at the very top of the file system.  
+Use "pwd" to print working directory.ArithmeticError.
+Use readlink -f relative_data.txt to find the absolute path to relative_data.txt.
+"""
+x = open('/home/ubuntu/workspace/files/relative_data.txt', 'r')
+
+relativeLines = f.read()
 f.close()
-print(lines)
+print(relativeLines)
+
+absoluteLines = x.read()
+x.close()
+print(absoluteLines)
